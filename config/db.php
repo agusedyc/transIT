233900f31d693978bc262354112d1791cc3b0c37
Agus Edy <edyagusc@gmail.com>
2019-10-20 21:46:18 +0700

    Config DB

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=mariadb;dbname=apps_docker',
     'username' => 'root',
-    'password' => '',
+    'password' => 'admin_db',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
