6c2de2b2ecca17bb35b3e956a2646cb0d05cb091
Agus Edy <edyagusc@gmail.com>
2020-01-21 21:17:54 +0700

    created simple registration

--- models/RegistrationForm.php
+++ models/RegistrationForm.php
@@ -44,21 +44,21 @@ class RegistrationForm extends BaseRegistrationForm
             // 'nimTrim'     => ['nim', 'trim'],
             // 'nimLength'   => ['nim', 'string', 'min' => 13, 'max' => 13],
             // 'nimPattern'  => ['nim', 'match', 'pattern' => '/^[G].[0-9][0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9]$/m'],
-            'nimRequired' => ['nim', 'required'],
-            'nimUnique'   => [
-                'nim',
-                'unique',
-                'targetClass' => $user,
-                'message' => Yii::t('user', 'This nim has already been taken'),
-            ],
+            // 'nimRequired' => ['nim', 'required'],
+            // 'nimUnique'   => [
+            //     'nim',
+            //     'unique',
+            //     'targetClass' => $user,
+            //     'message' => Yii::t('user', 'This nim has already been taken'),
+            // ],
             // full_name rules
-            'full_nameTrim'     => ['full_name', 'trim'],
-            'full_nameRequired' => ['full_name', 'required'],
+            // 'full_nameTrim'     => ['full_name', 'trim'],
+            // 'full_nameRequired' => ['full_name', 'required'],
 
             // username rules
             'usernameTrim'     => ['username', 'trim'],
             'usernameLength'   => ['username', 'string', 'min' => 3, 'max' => 255],
-            'usernamePattern'  => ['username', 'match', 'pattern' => $user::$usernameRegexp],
+            'usernamePattern'  => ['username', 'match', 'pattern' => '/^[G].[0-9][0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9]$/m'],
             'usernameRequired' => ['username', 'required'],
             'usernameUnique'   => [
                 'username',
@@ -89,42 +89,42 @@ class RegistrationForm extends BaseRegistrationForm
     {
         return [
             'item_name'=> Yii::t('user', 'Roles'),
-            'nim'      => Yii::t('user', 'NIM'),
-            'full_name'=> Yii::t('user', 'Full Name'),
+            // 'nim'      => Yii::t('user', 'NIM'),
+            // 'full_name'=> Yii::t('user', 'Full Name'),
             'email'    => Yii::t('user', 'Email'),
-            'username' => Yii::t('user', 'Username'),
+            'username' => Yii::t('user', 'NIM'),
             'password' => Yii::t('user', 'Password'),
         ];
     }
 
-    public function afterSave($isNew, $old)
-    {
-    	if ($isNew){
-	    	$model = $this->findModel($this->user->id);
-	        $authAssignment = new AuthAssignment([
-	            'user_id' => $model->id,
-	        ]);
+ //    public function afterSave($isNew, $old)
+ //    {
+ //    	if ($isNew){
+	//     	$model = $this->findModel($this->user->id);
+	//         $authAssignment = new AuthAssignment([
+	//             'user_id' => $model->id,
+	//         ]);
 
-	        $authAssignment->load(Yii::$app->request->post());
-	        // delete all role
-	        AuthAssignment::deleteAll(['user_id' => $model->id]);
-	        if (is_array($authAssignment->item_name)) {
-	            foreach ($authAssignment->item_name as $item) {
-	                if (!in_array($item, $authAssignments)) {
-	                    $authAssignment2 = new AuthAssignment([
-	                        'user_id' => $model->id,
-	                    ]);
-	                    $authAssignment2->item_name = $item;
-	                    $authAssignment2->created_at = time();
-	                    $authAssignment2->save();
+	//         $authAssignment->load(Yii::$app->request->post());
+	//         // delete all role
+	//         AuthAssignment::deleteAll(['user_id' => $model->id]);
+	//         if (is_array($authAssignment->item_name)) {
+	//             foreach ($authAssignment->item_name as $item) {
+	//                 if (!in_array($item, $authAssignments)) {
+	//                     $authAssignment2 = new AuthAssignment([
+	//                         'user_id' => $model->id,
+	//                     ]);
+	//                     $authAssignment2->item_name = $item;
+	//                     $authAssignment2->created_at = time();
+	//                     $authAssignment2->save();
 
-	                    $authAssignments = AuthAssignment::find()->where([
-	                        'user_id' => $model->id,
-	                    ])->column();
-	                }
-	            }
-	        }
-	    }
-	}
+	//                     $authAssignments = AuthAssignment::find()->where([
+	//                         'user_id' => $model->id,
+	//                     ])->column();
+	//                 }
+	//             }
+	//         }
+	//     }
+	// }
 
 }
