f3806c7142cfbbd880525720cbf2ee51357dc01a
Agus Edy <edyagusc@gmail.com>
2020-05-22 14:46:23 +0700

    added container about

--- views/site/about.php
+++ views/site/about.php
@@ -5,52 +5,55 @@
 use yii\helpers\Html;
 
 $this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <!-- <h1><?= Html::encode($this->title) ?></h1> -->
-
-    <!-- <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p> -->
-
-    <!-- <code><?= __FILE__ ?></code> -->
-    <div class="row">
-		<?php foreach ($pembimbing as $value): ?>
-		<div class="col-lg-3 col-xs-6">
-    		<div class="box box-widget widget-user">
-            <div class="widget-user-header bg-aqua-active">
-              <h3 class="widget-user-username"><?= $value->pembimbing ?></h3>
-              <h5 class="widget-user-desc"><?= $value->jabatan ?></h5>
-              <h5 class="widget-user-desc"><?= $value->tlp ?></h5>
-            </div>
-            <div class="widget-user-image">
-              <img class="img-circle" style="width:128px; height:128px" src="<?= Yii::$app->request->hostInfo.'/'.$value->foto ?>" alt="<?= $value->pembimbing ?>">
-            </div>
-            <!-- <div class="box-footer">
-              <div class="row">
-                <div class="col-sm-4 border-right">
-                  <div class="description-block">
-                    <h5 class="description-header">3,200</h5>
-                    <span class="description-text">SALES</span>
+    <div class="box box-default box-solid">
+      <div class="box-header with-border">
+        <h3 class="box-title"><?= Html::encode('Pengelola Trans IT USM') ?></h3>
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+        </div>
+      </div>
+      <div class="box-body" style="">
+        <div class="row">
+          <?php foreach ($pembimbing as $value): ?>
+          <div class="col-lg-3 col-xs-6">
+              <div class="box box-widget widget-user">
+                  <div class="widget-user-header bg-aqua-active">
+                    <h3 class="widget-user-username"><?= $value->pembimbing ?></h3>
+                    <h5 class="widget-user-desc"><?= $value->jabatan ?></h5>
+                    <h5 class="widget-user-desc"><?= $value->tlp ?></h5>
                   </div>
-                </div>
-                <div class="col-sm-4 border-right">
-                  <div class="description-block">
-                    <h5 class="description-header">13,000</h5>
-                    <span class="description-text">FOLLOWERS</span>
+                  <div class="widget-user-image">
+                    <img class="img-circle" style="width:128px; height:128px" src="<?= Yii::$app->request->hostInfo.'/'.$value->foto ?>" alt="<?= $value->pembimbing ?>">
                   </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="description-block">
-                    <h5 class="description-header">35</h5>
-                    <span class="description-text">PRODUCTS</span>
-                  </div>
-                </div>
-              </div>
-            </div> -->
-          </div>    		
-		</div>    		
-		<?php endforeach ?>    	
-    </div>
+                  <!-- <div class="box-footer">
+                    <div class="row">
+                      <div class="col-sm-4 border-right">
+                        <div class="description-block">
+                          <h5 class="description-header">3,200</h5>
+                          <span class="description-text">SALES</span>
+                        </div>
+                      </div>
+                      <div class="col-sm-4 border-right">
+                        <div class="description-block">
+                          <h5 class="description-header">13,000</h5>
+                          <span class="description-text">FOLLOWERS</span>
+                        </div>
+                      </div>
+                      <div class="col-sm-4">
+                        <div class="description-block">
+                          <h5 class="description-header">35</h5>
+                          <span class="description-text">PRODUCTS</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div> -->
+                </div>        
+          </div>        
+          <?php endforeach ?>     
+          </div>
+      </div>
+      </div>
 </div>
